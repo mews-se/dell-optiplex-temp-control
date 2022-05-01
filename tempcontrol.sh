8bf92fc27ecaf7c177e00a9dee0a17fd0f1ee6e9
mews-se <58894405+mews-se@users.noreply.github.com>
2022-05-01 02:50:18 +0200

    Update tempcontrol.sh

--- tempcontrol.sh
+++ tempcontrol.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 while sleep 2;
 do
-  read temp < /sys/class/hwmon/hwmon3/temp1_input
+  read temp < /sys/class/hwmon/hwmon3/temp1_input # change to your temperature sensor
   #echo $temp
-  if [ $temp \< 40000 ];
+  if [ $temp \< 40000 ]; # min temp
     then /usr/bin/i8kfan - 0 > /dev/null
   elif
-    [ $temp \< 45000 ];
+    [ $temp \< 45000 ]; # max temp
       then /usr/bin/i8kfan - 1 > /dev/null
   else
     /usr/bin/i8kfan - 2 > /dev/null
